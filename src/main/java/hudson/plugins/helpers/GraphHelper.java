916599b85dbc1e8c41f924461e344fb219dd6a8d
Arno Moonen <arno.moonen@altran.com>
2016-12-14 15:12:55 +0100

    Attempt to fix JENKINS-21511

--- src/main/java/hudson/plugins/helpers/GraphHelper.java
+++ src/main/java/hudson/plugins/helpers/GraphHelper.java
@@ -4,14 +4,9 @@ import hudson.model.AbstractBuild;
 import hudson.plugins.cppncss.AbstractBuildReport;
 import hudson.plugins.cppncss.parser.Statistic;
 import hudson.util.ChartUtil;
+import hudson.util.ChartUtil.NumberOnlyBuildLabel;
 import hudson.util.DataSetBuilder;
 import hudson.util.ShiftedCategoryAxis;
-import hudson.util.ChartUtil.NumberOnlyBuildLabel;
-
-import java.awt.Color;
-import java.io.IOException;
-import java.util.Collection;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryAxis;
@@ -26,6 +21,10 @@ import org.jfree.ui.RectangleInsets;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
+import java.awt.*;
+import java.io.IOException;
+import java.util.Collection;
+
 /**
  * TODO javadoc.
  *
@@ -33,17 +32,18 @@ import org.kohsuke.stapler.StaplerResponse;
  * @since 09-Jan-2008 21:30:15
  */
 public class GraphHelper {
-	
-	public interface CategoryDatasetBuilder {
-		CategoryDataset buildData();
-	}
-	
-	public interface DataCollector {
-		
-		long getCollectedNumber(AbstractBuildReport<?> action);
-		String getTitle();
-	}
-	
+
+    public interface CategoryDatasetBuilder {
+        CategoryDataset buildData();
+    }
+
+    public interface DataCollector {
+
+        long getCollectedNumber(AbstractBuildReport<?> action);
+
+        String getTitle();
+    }
+
     /**
      * Do not instantiate GraphHelper.
      */
@@ -65,21 +65,21 @@ public class GraphHelper {
     }
 
     public static JFreeChart buildChart(final AbstractBuild<?, ?> build, final Integer functionCcnViolationThreshold, final Integer functionNcssViolationThreshold) {
-    	
+
         final JFreeChart chart = ChartFactory.createStackedAreaChart(
                 null,                     // chart title
                 null,                     // unused
                 "Total Files",            // range axis label
-                buildDataset(build , new DataCollector() {
-					
-					public String getTitle() {
-						return "Files";
-					}
-					
-					public long getCollectedNumber(AbstractBuildReport action) {
-						return action.getResults().getFileResults().size();
-					}
-				}),      // data
+                buildDataset(build, new DataCollector() {
+
+                    public String getTitle() {
+                        return "Files";
+                    }
+
+                    public long getCollectedNumber(AbstractBuildReport action) {
+                        return action.getResults().getFileResults().size();
+                    }
+                }),      // data
                 PlotOrientation.VERTICAL, // orientation
                 true,                     // include legend
                 true,                     // tooltips
@@ -107,138 +107,141 @@ public class GraphHelper {
         // crop extra space around the graph
         plot.setInsets(new RectangleInsets(0, 0, 0, 5.0));
 
-        CategoryItemRenderer firstRender= new DefaultCategoryItemRenderer();
-        plot.setRenderer(firstRender); 
+        CategoryItemRenderer firstRender = new DefaultCategoryItemRenderer();
+        plot.setRenderer(firstRender);
 
         int index = 1;
-		//Second
+        //Second
         build_category(build, chart, index, Color.BLUE, "Non Commenting Source Statements", new CategoryDatasetBuilder() {
-			public CategoryDataset buildData() {
-				return buildDataset(build, new DataCollector() {
-					
-					public String getTitle() {
-						return "NCSS";
-					}
-					
-					public long getCollectedNumber(AbstractBuildReport action) {
-						return action.getTotals().getFileTotal().getNcss();
-					}
-				});
-			}
+            public CategoryDataset buildData() {
+                return buildDataset(build, new DataCollector() {
+
+                    public String getTitle() {
+                        return "NCSS";
+                    }
+
+                    public long getCollectedNumber(AbstractBuildReport action) {
+                        return action.getTotals().getFileTotal().getNcss();
+                    }
+                });
+            }
         });
-        
-        index ++;
-      //Third
+
+        index++;
+        //Third
         build_category(build, chart, index, Color.GREEN, "McCabe's Cyclomatic Number", new CategoryDatasetBuilder() {
-			public CategoryDataset buildData() {
-				return buildDataset(build, new DataCollector() {
-					
-					public String getTitle() {
-						return "CCN";
-					}
-					
-					public long getCollectedNumber(AbstractBuildReport action) {
-						return action.getTotals().getFileTotal().getCcn();
-					}
-				});
-			}
-		});
-        
-        index ++;
-      //Fouth
+            public CategoryDataset buildData() {
+                return buildDataset(build, new DataCollector() {
+
+                    public String getTitle() {
+                        return "CCN";
+                    }
+
+                    public long getCollectedNumber(AbstractBuildReport action) {
+                        return action.getTotals().getFileTotal().getCcn();
+                    }
+                });
+            }
+        });
+
+        index++;
+        //Fouth
         build_category(build, chart, index, Color.ORANGE, "Total Functions", new CategoryDatasetBuilder() {
-			public CategoryDataset buildData() {
-				return buildDataset(build, new DataCollector() {
-					
-					public String getTitle() {
-						return "Functions";
-					}
-					
-					public long getCollectedNumber(AbstractBuildReport action) {
-						return action.getTotals().getFileTotal().getFunctions();
-					}
-				});
-			}
-		});
-        
-        
-        index ++;
-      //Fifth
+            public CategoryDataset buildData() {
+                return buildDataset(build, new DataCollector() {
+
+                    public String getTitle() {
+                        return "Functions";
+                    }
+
+                    public long getCollectedNumber(AbstractBuildReport action) {
+                        return action.getTotals().getFileTotal().getFunctions();
+                    }
+                });
+            }
+        });
+
+
+        index++;
+        //Fifth
         build_category(build, chart, index, Color.CYAN, "CCN Violated Functions", new CategoryDatasetBuilder() {
-			public CategoryDataset buildData() {
-				return buildDataset(build, new DataCollector() {
-					
-					public String getTitle() {
-						return "CCNVF";
-					}
-					
-					public long getCollectedNumber(AbstractBuildReport action) {
-						Collection<Statistic> functionResults = action.getResults().getFunctionResults();
-						int ccnViolatedFunctions = 0;
-		            	
-		            	for (Statistic statistic : functionResults) {
-							if(statistic.getCcn() > functionCcnViolationThreshold.intValue())
-								ccnViolatedFunctions ++;
-						}
-						return ccnViolatedFunctions;
-					}
-				});
-			}
-		});
-        
-        index ++;
-      //Sixth
+            public CategoryDataset buildData() {
+                return buildDataset(build, new DataCollector() {
+
+                    public String getTitle() {
+                        return "CCNVF";
+                    }
+
+                    public long getCollectedNumber(AbstractBuildReport action) {
+                        Collection<Statistic> functionResults = action.getResults().getFunctionResults();
+                        int ccnViolatedFunctions = 0;
+                        if (functionCcnViolationThreshold != null) {
+                            for (Statistic statistic : functionResults) {
+                                if (statistic.getCcn() > functionCcnViolationThreshold)
+                                    ccnViolatedFunctions++;
+                            }
+                        }
+                        return ccnViolatedFunctions;
+                    }
+                });
+            }
+        });
+
+        index++;
+        //Sixth
         build_category(build, chart, index, Color.MAGENTA, "NCSS Violated Functions", new CategoryDatasetBuilder() {
-			public CategoryDataset buildData() {
-				return buildDataset(build, new DataCollector() {
-					
-					public String getTitle() {
-						return "NCSSVF";
-					}
-					
-					public long getCollectedNumber(AbstractBuildReport action) {
-						Collection<Statistic> functionResults = action.getResults().getFunctionResults();
-		            	int ncssViolatedFunctions = 0;
-		            	
-		            	for (Statistic statistic : functionResults) {
-							if(statistic.getNcss() > functionNcssViolationThreshold.intValue())
-								ncssViolatedFunctions ++;
-						}
-						return ncssViolatedFunctions;
-					}
-				});
-			}
-		});
-        
+            public CategoryDataset buildData() {
+                return buildDataset(build, new DataCollector() {
+
+                    public String getTitle() {
+                        return "NCSSVF";
+                    }
+
+                    public long getCollectedNumber(AbstractBuildReport action) {
+                        Collection<Statistic> functionResults = action.getResults().getFunctionResults();
+                        int ncssViolatedFunctions = 0;
+
+                        if (functionNcssViolationThreshold != null) {
+                            for (Statistic statistic : functionResults) {
+                                if (statistic.getNcss() > functionNcssViolationThreshold)
+                                    ncssViolatedFunctions++;
+                            }
+                        }
+                        return ncssViolatedFunctions;
+                    }
+                });
+            }
+        });
+
         return chart;
     }
 
-	private static void build_category(AbstractBuild<?, ?> build,
-			final JFreeChart chart, int index, Color color, String title, CategoryDatasetBuilder datasetBuilder) {
-		    NumberAxis axis= new NumberAxis(title);
-		    axis.setLabelPaint(color);
-		    axis.setAxisLinePaint(color);
-		    axis.setTickLabelPaint(color);        
-		    CategoryPlot categoryPlot = chart.getCategoryPlot();
-		    categoryPlot.setRangeAxis(index, axis);        
-		    categoryPlot.setDataset(index, datasetBuilder.buildData());
-		    categoryPlot.mapDatasetToRangeAxis(index,index);
-		    categoryPlot.mapDatasetToDomainAxis(index,0);
-		    CategoryItemRenderer rendu= new DefaultCategoryItemRenderer();        
-		    rendu.setBasePaint(color);
-		    categoryPlot.setRenderer(index,rendu);
-	}
-    
-	private static CategoryDataset buildDataset(AbstractBuild<?, ?> build, DataCollector collector) {
-    	DataSetBuilder<String, NumberOnlyBuildLabel> builder = new DataSetBuilder<String, NumberOnlyBuildLabel>();
-    	
-    	for (AbstractBuild<?, ?> lastBuild = build; lastBuild != null; lastBuild = lastBuild.getPreviousBuild()) { 
-        	ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(lastBuild);
+    private static void build_category(AbstractBuild<?, ?> build,
+                                       final JFreeChart chart, int index, Color color, String title, CategoryDatasetBuilder datasetBuilder) {
+        NumberAxis axis = new NumberAxis(title);
+        axis.setLabelPaint(color);
+        axis.setAxisLinePaint(color);
+        axis.setTickLabelPaint(color);
+        CategoryPlot categoryPlot = chart.getCategoryPlot();
+        categoryPlot.setRangeAxis(index, axis);
+        categoryPlot.setDataset(index, datasetBuilder.buildData());
+        categoryPlot.mapDatasetToRangeAxis(index, index);
+        categoryPlot.mapDatasetToDomainAxis(index, 0);
+        CategoryItemRenderer rendu = new DefaultCategoryItemRenderer();
+        rendu.setBasePaint(color);
+        categoryPlot.setRenderer(index, rendu);
+    }
+
+    private static CategoryDataset buildDataset(AbstractBuild<?, ?> build, DataCollector collector) {
+        DataSetBuilder<String, NumberOnlyBuildLabel> builder = new DataSetBuilder<String, NumberOnlyBuildLabel>();
+
+        for (AbstractBuild<?, ?> lastBuild = build; lastBuild != null; lastBuild = lastBuild.getPreviousBuild()) {
+            ChartUtil.NumberOnlyBuildLabel label = new ChartUtil.NumberOnlyBuildLabel(lastBuild);
             AbstractBuildReport action = lastBuild.getAction(AbstractBuildReport.class);
             if (action != null) {
-            	builder.add(collector.getCollectedNumber(action), collector.getTitle(), label);
+                builder.add(collector.getCollectedNumber(action), collector.getTitle(), label);
             }
         }
         return builder.build();
-	}
+    }
 }
